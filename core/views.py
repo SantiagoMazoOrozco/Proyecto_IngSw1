1299b7b32775907c39a0b5c0c6a669cc8cb95ba6
SantiagoMazoOrozco <smazoo@unal.edu.co>
2025-06-15 18:20:08 -0500

    se corrigen maricadas que a los otros les sale mal y a mi bien pero igualemnte me toca corregir a mi

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from .models import Perfil
 from .forms import AvatarForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 @login_required
 def cambiar_avatar(request):
@@ -14,3 +16,14 @@ def cambiar_avatar(request):
     else:
         form = AvatarForm(instance=perfil)
     return render(request, 'cambiar_avatar.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Cambia 'home' por la vista que prefieras tras el registro
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
